734986abb27260d8da1c961a9b84010b9fe4e00f
Jon <jchica72@gmail.com>
2018-11-17 14:31:41 +0100

    Add second script file and import

--- 01-basic-webpack/src/index.ts
+++ 01-basic-webpack/src/index.ts
@@ -1,6 +1,18 @@
 const logoImg = require('./content/logo_2.png');
+import { summaryClass } from './sideService';
 
 const img = document.createElement('img');
 img.src = logoImg;
 
 document.getElementById('imgContainer').appendChild(img);
+
+const listSummary = document.createElement('ul');
+
+summaryClass.forEach((student) => {
+  let newStudent = document.createElement('li');
+  let studentRow = `Name: ${student.name} | Avg: ${student.averageCalification} | High: ${student.highestCalification}`;
+  newStudent.append(studentRow);
+  listSummary.appendChild(newStudent);
+});
+
+document.getElementById('list-summary').appendChild(listSummary);
